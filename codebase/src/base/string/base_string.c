e1688b9ebcc8a4e31df350970c3524df99785bc5
Westsi <76999267+Westsi@users.noreply.github.com>
2023-04-29 18:16:18 +0100

    started implementation of strings

--- codebase/src/base/string/base_string.c
+++ codebase/src/base/string/base_string.c
@@ -1,3 +1,50 @@
 /* date = 29 Apr 2023 17:33 */
 
 #include "base_string.h"
+#include "../base_macros.h"
+#include "../base_types.h"
+
+String8 str8(U8 *str, U64 size) {
+    String8 result = {str, size};
+    return result;
+}
+
+String8 str8_range(U8 *first, U8 *opl) {
+    // Implement
+}
+
+String8 str8_cstring(U8 *cstr) {
+    // Implement
+}
+
+
+String8 str8_prefix(String8 str, U64 size) {
+    U64 size_clamped = ClampTop(size, str.size);
+    String8 result = {str.str, size_clamped};
+    return result;
+}
+
+String8 str8_chop(String8 str, U64 amount) {
+    U64 amount_clamped = ClampTop(amount, str.size);
+    U64 remaining_size = str.size - amount_clamped;
+    String8 result = {str.str, remaining_size};
+    return result;
+}
+
+String8 str8_postfix(String8 str, U64 size) {
+    U64 size_clamped = ClampTop(size, str.size);
+    U64 skip_to = str.size - size_clamped;
+    String8 result = {str.str + skip_to, size_clamped};
+    return result;
+}
+
+String8 str8_skip(String8 str, U64 amount) {
+    U64 amount_clamped = ClampTop(amount, str.size);
+    U64 remaining_size = str.size - amount_clamped;
+    String8 result = {str.str + amount_clamped, remaining_size};
+    return result;
+}
+
+String8 str8_substr(String8 str, U64 first, U64 opl) {
+    // Implement
+}
--- codebase/src/base/string/base_string.h
+++ codebase/src/base/string/base_string.h
@@ -3,4 +3,40 @@
 #ifndef BASE_STRING_H_
 #define BASE_STRING_H_
 
+#include "../base_types.h"
+
+// String Types
+
+typedef struct String8 {
+    U8 *str;
+    U64 size;
+} String8;
+
+typedef struct String8Node {
+    int *next; // CAST TO String8Node before use!!!
+    String8 string;
+} String8Node;
+
+typedef struct String8List {
+    String8Node *first;
+    String8Node *last;
+    U64 node_count;
+    U64 total_size;
+} String8List;
+
+// String Functions
+String8 str8(U8 *str, U64 size);
+String8 str8_range(U8 *first, U8 *opl);
+String8 str8_cstring(U8 *cstr);
+
+#define str8_lit(s) str8((U8*)(s), sizeof(s) - 1)
+
+String8 str8_prefix(String8 str, U64 size);
+String8 str8_chop(String8 str, U64 amount);
+String8 str8_postfix(String8 str, U64 size);
+String8 str8_skip(String8 str, U64 amount);
+String8 str8_substr(String8 str, U64 first, U64 opl);
+
+#define str8_expand(s) (int)((s).size), ((s).str)
+
 #endif //BASE_STRING_H
